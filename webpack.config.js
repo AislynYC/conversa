bfcb7bd3ac4aa6b985742d7f3dc88a43ffea07ab
AislynYC <chuang.yuchun@gmail.com>
2020-01-22 10:49:19 +0800

    Set-up environment

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
