428a5383a6bbd08c305e3bce4322f7365ede1f04
AislynYC <chuang.yuchun@gmail.com>
2020-01-29 22:48:03 +0800

    Add testing slides

--- src/components/CurrentSld/CurrentSld.js
+++ src/components/CurrentSld/CurrentSld.js
@@ -4,6 +4,13 @@ import "./style.css";
 class CurrentSld extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sldNo: "1",
+      qContent: "Question content for test 1",
+      qType: "Question type for test 1",
+      resContent: "Result content for test 1",
+      resType: "Result type for test 1"
+    };
   }
   render() {
     return (
--- src/components/SldSelector/SldSelector.js
+++ src/components/SldSelector/SldSelector.js
@@ -3,10 +3,47 @@ import "./style.css";
 class SldSelector extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      slds: [
+        {
+          sldNo: "1",
+          qContent: "Question content for test 1",
+          qType: "Question type for test 1",
+          resContent: "Result content for test 1",
+          resType: "Result type for test 1"
+        },
+        {
+          sldNo: "2",
+          qContent: "Question content for test 2",
+          qType: "Question type for test 2",
+          resContent: "Result content for test 2",
+          resType: "Result type for test 2"
+        },
+        {
+          sldNo: "3",
+          qContent: "Question content for test 3",
+          qType: "Question type for test 3",
+          resContent: "Result content for test 3",
+          resType: "Result type for test 3"
+        }
+      ]
+    };
   }
 
   render() {
-    return <div id="sld-selector">test</div>;
+    let sldsItems = this.state.slds.map((item, index) => {
+      let sldNo = index + 1;
+      return (
+        <div className="sld-item">
+          <div>{sldNo}</div>
+          <div className="sld">
+            <div>{item.qContent}</div>
+            <div>{item.resContent}</div>
+          </div>
+        </div>
+      );
+    });
+    return <div id="sld-selector">{sldsItems}</div>;
   }
 }
 export default SldSelector;
