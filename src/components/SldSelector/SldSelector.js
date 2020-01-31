4bb9c3d415eca50eb33c70972175c653ef3249fc
AislynYC <chuang.yuchun@gmail.com>
2020-01-31 08:51:31 +0800

    Add slide selection logic

--- src/components/SldSelector/SldSelector.js
+++ src/components/SldSelector/SldSelector.js
@@ -6,43 +6,65 @@ class SldSelector extends React.Component {
     this.state = {
       slds: [
         {
-          sldNo: "1",
+          id: "01",
           qContent: "Question content for test 1",
           qType: "Question type for test 1",
           resContent: "Result content for test 1",
-          resType: "Result type for test 1"
+          resType: "Result type for test 1",
+          selected: false
         },
         {
-          sldNo: "2",
+          id: "02",
           qContent: "Question content for test 2",
           qType: "Question type for test 2",
           resContent: "Result content for test 2",
-          resType: "Result type for test 2"
+          resType: "Result type for test 2",
+          selected: false
         },
         {
-          sldNo: "3",
+          id: "03",
           qContent: "Question content for test 3",
           qType: "Question type for test 3",
           resContent: "Result content for test 3",
-          resType: "Result type for test 3"
+          resType: "Result type for test 3",
+          selected: false
         }
       ]
     };
   }
 
+  selectSld = (e, selected, selectedId) => {
+    if (selected === false) {
+      this.setState(
+        prevState => ({
+          slds: prevState.slds.map(sld => {
+            return sld.id === selectedId
+              ? {...sld, selected: true}
+              : {...sld, selected: false};
+          })
+        }),
+        () => console.log(this.state.slds)
+      );
+    }
+  };
+
   render() {
     let sldsItems = this.state.slds.map((item, index) => {
-      let sldNo = index + 1;
       return (
-        <div className="sld-item">
-          <div>{sldNo}</div>
-          <div className="sld">
+        <div className="sld-item" key={index + 1}>
+          <div>{index + 1}</div>
+          <div
+            className="sld"
+            onClick={e => {
+              this.selectSld(e, item.selected, item.id);
+            }}>
             <div>{item.qContent}</div>
             <div>{item.resContent}</div>
           </div>
         </div>
       );
     });
+
     return <div id="sld-selector">{sldsItems}</div>;
   }
 }
