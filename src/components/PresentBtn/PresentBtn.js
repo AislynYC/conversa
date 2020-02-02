164ea9b75cad5b4c340aa6c3d604a698abe9f076
AislynYC <chuang.yuchun@gmail.com>
2020-02-02 19:51:55 +0800

    Clear unnecessary code

--- src/components/PresentBtn/PresentBtn.js
+++ src/components/PresentBtn/PresentBtn.js
@@ -3,18 +3,17 @@ import {FormattedMessage} from "react-intl";
 import "./style.css";
 const PresentBtn = () => {
   let present = () => {
-    const elem = document.getElementById("current-sld");
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-      elem.msRequestFullscreen();
-    } else if (elem.mozRequestFullScreen) {
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) {
-      elem.webkitRequestFullscreen();
+    const el = document.getElementById("current-sld");
+    if (el.requestFullscreen) {
+      el.requestFullscreen();
+    } else if (el.msRequestFullscreen) {
+      el.msRequestFullscreen();
+    } else if (el.mozRequestFullScreen) {
+      el.mozRequestFullScreen();
+    } else if (el.webkitRequestFullscreen) {
+      el.webkitRequestFullscreen();
     }
   };
-
   return (
     <button onClick={present} id="present-btn">
       <FormattedMessage id="app.present" />
