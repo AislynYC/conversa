cb87988bdff978f3e97e675c503f81d2acd10db8
AislynYC <chuang.yuchun@gmail.com>
2020-02-03 17:41:46 +0800

    Update language package

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,2 +1,2 @@
-const en = {"app.present": "Present"};
+const en = {"app.present": "Present", "add.sld": "Add Slide"};
 export default en;
--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -1,2 +1,2 @@
-const zh_TW = {"app.present": "開始投影"};
+const zh_TW = {"app.present": "開始投影", "add.sld": "新投影片"};
 export default zh_TW;
