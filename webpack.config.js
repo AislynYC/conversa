1aa6aa0ac5b9e94261f83f59b468b9d727b1aa84
AislynYC <chuang.yuchun@gmail.com>
2020-02-05 13:48:25 +0800

    Fix router refreshing error while using devServer

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   module: {
     rules: [
@@ -20,6 +21,7 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
+    historyApiFallback: true
   }
 };
