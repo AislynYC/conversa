f6dfcdaa6fda872e31a1663c9654ae5a2850c928
AislynYC <chuang.yuchun@gmail.com>
2020-02-10 22:06:25 +0800

    add file-loader, fontawsome & material-ui

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
       }
     ]
   },
