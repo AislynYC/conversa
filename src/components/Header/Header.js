f8bbca6ffaf09cdca581ea27d191dab21913fbac
AislynYC <chuang.yuchun@gmail.com>
2020-02-10 22:06:38 +0800

    Add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import LangBtn from "../LangBtn/LangBtn.js";
 import PresentBtn from "../PresentBtn/PresentBtn.js";
+import logo from "../../img/conversa.png";
 import "./style.css";
 const Header = props => {
   return (
     <div className="header">
-      <div className="logo">CONVERSA</div>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
       <LangBtn locale={props.locale} setLocale={props.setLocale} />
       <PresentBtn />
     </div>
