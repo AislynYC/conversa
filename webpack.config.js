895d9f412ab54544521419b18a47d14f8e0ee5c3
AislynYC <chuang.yuchun@gmail.com>
2020-02-29 16:13:31 +0800

    Install and settle up html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -45,5 +46,12 @@ module.exports = {
   devServer: {
     contentBase: "./dist",
     historyApiFallback: true
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico"
+    })
+  ]
 };
