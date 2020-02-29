16c66089f86f19b2d5c2a3d427a70113282ef8a6
AislynYC <chuang.yuchun@gmail.com>
2020-02-29 11:28:49 +0800

    Improve loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,18 +1,24 @@
-import React, {useState} from "react";
+import React from "react";
 import "./style.css";
 
-const Loading = () => {
+const Loading = props => {
+  console.log(props);
+  let loadingCircle = null;
+  if (props.match.url.includes("/audi/")) {
+    console.log("ya");
+    loadingCircle = <div className="loading-circle"></div>;
+  }
   return (
     <div className="wrapper">
-      <div className="box">
-        <div className="stem">
-          <div className="leaf leaf01"></div>
-          <div className="leaf leaf02">
-            <div className="leaf02-green"></div>
-          </div>
-          <div className="leaf leaf03">
-            <div className="leaf03-light"></div>
-          </div>
+      <div className="succulent-box">
+        {loadingCircle}
+        <div className="stem"></div>
+        <div className="leaf leaf01"></div>
+        <div className="leaf leaf02">
+          <div className="leaf02-green"></div>
+        </div>
+        <div className="leaf leaf03">
+          <div className="leaf03-light"></div>
         </div>
         <div className="pot"></div>
         <div className="pot-top"></div>
